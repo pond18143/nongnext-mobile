25a4d9cf188dcc056472edc613cb5c3ce59f1cf3
pond18143 <58029432+pond18143@users.noreply.github.com>
2021-11-29 01:47:26 +0700

    del ignore

--- linebot.js
+++ linebot.js
@@ -37,7 +37,7 @@ function handleMessageEvent(event) {
     return client.replyMessage(event.replyToken, msg);
 }
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function () {
     console.log('run at port', app.get('port'));
