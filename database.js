69c0f8e83ec3b61206fe06d9c0c7da86927f230b
pond18143 <58029432+pond18143@users.noreply.github.com>
2021-11-29 00:23:56 +0700

    commit

--- database.js
+++ database.js
@@ -1,27 +1,27 @@
-var sql = require("mssql");
-// config for your database
-var config = {
-    user: 'sa',
-    password: 'P@ssw0rd',
-    server: '68.183.186.117',
-    database: 'nongnext',
-    options: {
-        encrypt: false
-    }
-  };
+// var sql = require("mssql");
+// // config for your database
+// var config = {
+//     user: 'sa',
+//     password: 'P@ssw0rd',
+//     server: '68.183.186.117',
+//     database: 'nongnext',
+//     options: {
+//         encrypt: false
+//     }
+//   };
 
-var err = sql.connect(config)
-if (err) console.log(err);
+// var err = sql.connect(config)
+// if (err) console.log(err);
 
-async function getUser() {
-    var request = new sql.Request();
-    var command = await request.query(`SELECT *
-        FROM nongnext.dbo.brand`);
-        console.log(command)
-    return 
-}
+// async function getUser() {
+//     var request = new sql.Request();
+//     var command = await request.query(`SELECT *
+//         FROM nongnext.dbo.brand`);
+//         console.log(command)
+//     return 
+// }
 
-getUser;
+// getUser;
 
 // const sql = require('mssql')
 // const sqlConfig = {
--- linebot.js
+++ linebot.js
@@ -1,158 +1,36 @@
-const express = require('express');
-const line = require('@line/bot-sdk');
+// Echo reply
 
-require('dotenv').config();
-
-const app = express();
-
-const config = {
-    channelAccessToken: 'YaeQhCzNLf9UjpO4UiiX3HRlZ8iixRqaumnJataQALX/bjQY3Lvz/yrY6Ce/1NTZGjP9+CDb3TLWrYjj2CaPirkVQwjJCs4oZbtOJrqKjPlSG1xuK4TixwaL0zXRsqtfm+YFGm8A/RmKK7dASoJ11gdB04t89/1O/w1cDnyilFU=',
-    channelSecret: 'e7afaa18967de97406ab281db92304ae'
-};
-
-const client = new line.Client(config);
-
-app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
-});
-
-function handleEvent(event) {
-
-    console.log(event);
-    if (event.type === 'message' && event.message.type === 'text') {
-        handleMessageEvent(event);
-    } else {
-        return Promise.resolve(null);
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
+    res.sendStatus(200)
+})
+app.listen(port)
+function reply(reply_token, msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {YaeQhCzNLf9UjpO4UiiX3HRlZ8iixRqaumnJataQALX/bjQY3Lvz/yrY6Ce/1NTZGjP9+CDb3TLWrYjj2CaPirkVQwjJCs4oZbtOJrqKjPlSG1xuK4TixwaL0zXRsqtfm+YFGm8A/RmKK7dASoJ11gdB04t89/1O/w1cDnyilFU=}'
     }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: msg
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
 }
-
-function handleMessageEvent(event) {
-    var msg = {
-        type: 'text',
-        text: 'สวัสดีครัช'
-    };
-
-    var eventText = event.message.text.toLowerCase();
-
-    if (eventText === 'image') {
-        msg = {
-            'type': 'image',
-            'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
-            'previewImageUrl': 'https://images.performgroup.com/di/library/GOAL/a6/bb/fifa-18-ronaldo_lx3r88bpjpk91re36ukdgomrj.jpg?t=2027563652&w=620&h=430'
-        }
-    } else if (eventText === 'location') {
-        msg = {
-            "type": "location",
-            "title": "my location",
-            "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-            "latitude": 35.65910807942215,
-            "longitude": 139.70372892916203
-        }
-    } else if (eventText === 'template button') {
-        msg = {
-            "type": "template",
-            "altText": "this is a buttons template",
-            "template": {
-                "type": "buttons",
-                "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                "title": "Menu",
-                "text": "Please select",
-                "actions": [{
-                    "type": "postback",
-                    "label": "Buy",
-                    "data": "action=buy&itemid=123"
-                }, {
-                    "type": "postback",
-                    "label": "Add to cart",
-                    "data": "action=add&itemid=123"
-                }, {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "http://example.com/page/123"
-                }]
-            }
-        }
-    } else if (eventText === 'template confirm') {
-        msg = {
-            "type": "template",
-            "altText": "this is a confirm template",
-            "template": {
-                "type": "confirm",
-                "text": "Are you sure?",
-                "actions": [{
-                    "type": "message",
-                    "label": "Yes",
-                    "text": "yes"
-                }, {
-                    "type": "message",
-                    "label": "No",
-                    "text": "no"
-                }]
-            }
-        }
-    } else if (eventText === 'carousel') {
-        msg = {
-            "type": "template",
-            "altText": "this is a carousel template",
-            "template": {
-                "type": "carousel",
-                "columns": [
-                    {
-                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                        "title": "this is menu",
-                        "text": "description",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "Buy",
-                                "data": "action=buy&itemid=111"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Add to cart",
-                                "data": "action=add&itemid=111"
-                            },
-                            {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "http://example.com/page/111"
-                            }
-                        ]
-                    },
-                    {
-                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                        "title": "this is menu",
-                        "text": "description",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "Buy",
-                                "data": "action=buy&itemid=222"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Add to cart",
-                                "data": "action=add&itemid=222"
-                            },
-                            {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "http://example.com/page/222"
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
-
-    return client.replyMessage(event.replyToken, msg);
-}
-
-app.set('port', (process.env.PORT || 5000));
-
-app.listen(app.get('port'), function () {
-    console.log('run at port', app.get('port'));
-});
